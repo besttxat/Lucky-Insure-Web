11a8031d2379a0e0a1d4b8ee9586087512a027da
besttxat <besttxat@gmail.com>
2025-09-18 23:59:43 +0700

    ad  event Click

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 export default function AppWelcome() {
     const title = "Welcome to cosci";
     const year = <p>2025</p>;
     const isShow = true;
-
+    const handleClick = () => {
+        alert("Hello TypeScript");
+    }
 
     return (
         <div>
             <p>{title.toLocaleUpperCase()}</p>
+            <button className="bg-blue-700 p-3 rounded-lg" onClick={handleClick}>press button</button>
             {year}
             {
                 isShow && <p>Date: 10/10/1998</p>
