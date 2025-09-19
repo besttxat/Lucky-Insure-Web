8b2c3a2f5f7a5de61a3ced8d647c6c80a60c4d09
besttxat <besttxat@gmail.com>
2025-09-19 15:22:01 +0700

    add sahdcnui

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 type AppWelcomeProps = {
@@ -21,6 +22,7 @@ export default function AppWelcome({ headTitle,isShow }: AppWelcomeProps) {
         <div>
             <h1>{headTitle}</h1>
             <p>{title.toLocaleUpperCase()}</p>
+            <Button onClick={handleClick}> Click me</Button>
             <button className="bg-blue-700 p-3 rounded-lg" onClick={handleClick}>press button</button>
             {year}
             {
