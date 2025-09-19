69949ae6813a68de7d7426bae2457302730dbd54
besttxat <besttxat@gmail.com>
2025-09-19 17:38:37 +0700

    add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import FAQ01 from "@/components/faq-01/faq-01";
+import Features01Page from "@/components/features-01/features-01";
+import Footer05Page from "@/components/footer-05/footer-05";
 import Hero02 from "@/components/hero-02/hero-02";
 
 export default function Home() {
   return (
     <div>
       <Hero02 />
+
+      <Features01Page />
+
+      <FAQ01 />
+      
+      <Footer05Page />
     </div>
   );
 }
